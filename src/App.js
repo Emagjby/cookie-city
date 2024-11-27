7995345f75210f583e16a08bf69f9ea03fd62dbb
Emagjby <emagjby@gmail.com>
2024-11-27 22:31:47 +0200

    Finalized project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App(props) {
+  return (
+    <div className="container">
+      <div className="section"></div>
+      <div className="section"></div>
+      <div className="section"></div>
+      <div className="navbar"></div>
+    </div>
+  );
+}
+
+export default App;
